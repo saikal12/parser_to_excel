fa9ddee38071d71f6dfe562ea29cd3484f6c746d
Сулайманова Сайкал <sulaymanovasaykal2003gmail.com>
2024-10-24 23:58:12 +0300

    fix math regex

--- parser_to_excel.py
+++ parser_to_excel.py
@@ -5,21 +5,28 @@ import pandas as pd
 def parse_md_to_excel(md_file, excel_file):
     with open(md_file, 'r', encoding='utf-8') as md:
         content = md.read()
-        q_pattern = r'([BC]\d+)\s+(.+?)(?=\n\s*[BC]\d+|\n\s*##|\n\s*#|\Z)'
+        part_pattern = r'#+ ЧАСТЬ (\d+)'
+        q_pattern = r'([BCВ]\d+)\s+(.+?)(?=\n\s*[BCВ]\d+|\Z)'
         imag_pattern = r'!\[.*\]\((.*?)\)'
-    questions = re.findall(q_pattern, content, re.DOTALL)
-    print(questions)
     data = []
-    for question in questions:
-        q_number = question[0]
-        q_text = question[1].strip()
-        images = re.findall(imag_pattern, q_text)
-        clean_text = re.sub(imag_pattern, '', q_text).strip()
-        data.append(
-            [q_number, clean_text, ', '.join(images) if images else '']
-            )
+    parts = re.split(part_pattern, content)
+    for i in range(1, len(parts), 2):
+        part_number = parts[i].strip()
+        part_content = parts[i+1]
+        questions = re.findall(q_pattern, part_content, re.DOTALL)
 
-    df = pd.DataFrame(data, columns=['Номер вопроса', 'Вопрос', 'Рисунок'])
+        for question in questions:
+            q_number = question[0]
+            q_text = question[1].strip()
+            images = re.findall(imag_pattern, q_text)
+            clean_text = re.sub(imag_pattern, '', q_text).strip()
+            data.append(
+                [part_number, q_number, clean_text,
+                    ', '.join(images) if images else '']
+                )
+
+    df = pd.DataFrame(data, columns=['Часть', 'Номер вопроса',
+                                     'Вопрос', 'Рисунок'])
     df.to_excel(excel_file, index=False)
 
 
